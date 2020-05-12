942a5740ea33b48ebf9826f3b343c49e32d3ab16
JangGeonil <>
2020-05-12 22:41:08 +0900

    라우터 링크로 분리작업 햣 ㅔㅕ

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <h2>Start Vue Code</h2>
+    <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
 export default {
   name: "app",
+  components: { Navbar },
   data() {
     return {
       msg: "Welcome to Your Vue.js App"
