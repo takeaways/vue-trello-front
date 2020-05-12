a917905193e25aaeb4fe03ee38a95f8dd4b8f889
JangGeonil <>
2020-05-12 22:52:52 +0900

    동적 라우터 매칭

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Home from "../components/Home";
 import Login from "../components/Login";
 import NotFound from "../components/NotFound";
+import Board from "../components/Board";
 
 
 Vue.use(VueRouter);
@@ -14,6 +15,7 @@ const router = new VueRouter({
     routes: [
         { path: '/', component: Home },
         { path: '/login', component: Login },
+        { path: '/b/:bid', component: Board },
         { path: '*', component: NotFound }
     ]
 })
